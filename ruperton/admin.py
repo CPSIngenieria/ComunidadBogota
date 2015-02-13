5c880f74d684876ef389244ddeab22f3e704c8d2
AndresCW <andres@cpsingenieria.co>
2015-02-13 13:13:45 -0500

    Modificados los modelos del ruperton

--- ruperton/admin.py
+++ ruperton/admin.py
@@ -1,39 +1,64 @@
 from django.contrib import admin
-from .models import Sorteo, Concursante
+from .models import Sorteo, Concursante, Residente, Compra, Ganador
+
+class ResidenteAdmin(admin.ModelAdmin):
+
+	list_display = ('nombre', 'cedula', 'correo', 'direccion', 'telefono')
+
+class CompraAdmin(admin.ModelAdmin):
+
+	list_display = ('residente','monto','establecimiento','fecha_registro')
 
 class SorteoAdmin(admin.ModelAdmin):
 
 	fieldsets = [
-		('Informacion del sorteo', {'fields':['estado_sorteo']}),
+		('Informacion del sorteo', 
+			{'fields':
+				['nombre','estado_sorteo','fecha_inicio_registro_compras','fecha_cierre_registro_compras']
+			}
+		),
 		('Informacion fecha 1', 
 			{'fields':
-				['fecha_sorteo_1','premio_fecha_1','numero_ganador_1_fecha_1',
-				'numero_ganador_2_fecha_1','numero_ganador_3_fecha_1','estado_fecha_1']
+				['fecha_sorteo_1','premio_sorteo_1','numero_ganador_1_sorteo_1',
+				'numero_ganador_2_sorteo_1','numero_ganador_3_sorteo_1','estado_sorteo_1',
+				'ganador_sorteo_1']
 			}
 		),
 		('Informacion fecha 2', 
 			{'fields':
-				['fecha_sorteo_2','premio_fecha_2','numero_ganador_1_fecha_2',
-				'numero_ganador_2_fecha_2','numero_ganador_3_fecha_2','estado_fecha_2']
+				['fecha_sorteo_2','premio_sorteo_2','numero_ganador_1_sorteo_2',
+				'numero_ganador_2_sorteo_2','numero_ganador_3_sorteo_2','estado_sorteo_2',
+				'ganador_sorteo_2']
 			}
 		),
-		('Informacion fecha 3', 
+		('Informacion premio por compras registradas', 
 			{'fields':
-				['fecha_sorteo_3','premio_fecha_3','numero_ganador_1_fecha_3',
-				'numero_ganador_2_fecha_3','numero_ganador_3_fecha_3','estado_fecha_3']
+				['premio_registro_compras','ganador_registro_compras']
 			}
 		),
 	]
 
-	list_display = ('nombre_sorteo','fecha_creacion','estado_sorteo',)
+	list_display = (
+		'nombre',
+		'fecha_inicio_registro_compras',
+		'estado_sorteo',
+		'fecha_cierre_registro_compras'
+		)
 
 class ConcursanteAdmin(admin.ModelAdmin):
 
 	list_display = (
-		'nombre','correo','direccion','telefono','establecimiento','acepto_terminos','sorteo',
+		'nombre_concursante','correo_concursante','establecimiento','acepto_terminos','sorteo',
 	)
 
-	search_fields = ['nombre','correo','establecimiento']
+	search_fields = ['nombre_concursante','correo_concursante','establecimiento']
+
+class GanadorAdmin(admin.ModelAdmin):
+
+	list_display = ('residente','foto_admin')
 
 admin.site.register(Sorteo, SorteoAdmin)
 admin.site.register(Concursante, ConcursanteAdmin)
+admin.site.register(Residente, ResidenteAdmin)
+admin.site.register(Compra, CompraAdmin)
+admin.site.register(Ganador, GanadorAdmin)
--- ruperton/models.py
+++ ruperton/models.py
@@ -3,6 +3,41 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Residente(models.Model):
+	nombre = models.CharField( max_length=100)
+	cedula = models.CharField( max_length=100)
+	correo = models.EmailField(unique=True)
+	direccion = models.CharField( max_length=100)
+	telefono = models.CharField( max_length=100)
+
+	def __unicode__(self):
+		return self.nombre + " - " + self.correo
+
+class Compra(models.Model):
+	fecha_registro = models.DateTimeField(auto_now_add=True)
+	establecimiento = models.CharField(max_length=255)
+	monto = models.PositiveIntegerField()
+	residente = models.ForeignKey(Residente)
+
+	def __unicode__(self):
+		return self.residente.nombre + ' - ' + '{:,}'.format(self.monto)
+
+	def nombre_residente(self):
+		return self.residente.nombre
+
+class Ganador(models.Model):
+	residente = models.ForeignKey(Residente)
+	foto = models.ImageField(upload_to='fotos_ganadores')
+
+	def __unicode__(self):
+		return self.residente.nombre
+
+	def foto_admin(self):
+		return """
+		<img src='%s' width='300' height='200' />
+		""" % self.foto.url
+	foto_admin.allow_tags = True
+
 class Sorteo(models.Model):
 	
 	JUEGO='JU'
@@ -14,96 +49,103 @@ class Sorteo(models.Model):
 		(ACUMULADO, 'Acumulado'),
 	)
 
-	JUGANDO='JU'
-	INSCRIPCIONES='IN'
-	CERRADO='CE'
-	ESTADOS_SORTEOS = (
+	JUGANDO='Jugando'
+	CERRADO='Cerrado'
+	ESTADO_SORTEO = (
 		(JUGANDO, 'En juego'),
-		(INSCRIPCIONES, 'En inscripciones'),
 		(CERRADO, 'Cerrado'),
 	)
 
-	fecha_creacion = models.DateField( auto_now_add = True )
+	estado_sorteo = models.CharField(max_length=50, choices=ESTADO_SORTEO, default=JUGANDO)
+	nombre = models.CharField(max_length=100)
+	fecha_inicio_registro_compras = models.DateTimeField()
+	fecha_cierre_registro_compras = models.DateTimeField()
+	
 	fecha_sorteo_1 = models.DateField( blank = True )
+	estado_sorteo_1 = models.CharField(max_length=2, choices=ESTADOS_FECHAS, default=JUEGO)
+	premio_sorteo_1 = models.PositiveIntegerField()
+	numero_ganador_1_sorteo_1 = models.PositiveIntegerField(blank=True, null=True)
+	numero_ganador_2_sorteo_1 = models.PositiveIntegerField(blank=True, null=True)
+	numero_ganador_3_sorteo_1 = models.PositiveIntegerField(blank=True, null=True)
+	ganador_sorteo_1 = models.ManyToManyField(Ganador, related_name='ganador_sorteo_1', blank=True)
+	
 	fecha_sorteo_2 = models.DateField( blank = True )
-	fecha_sorteo_3 = models.DateField( blank = True )
-	premio_fecha_1 = models.PositiveIntegerField()
-	premio_fecha_2 = models.PositiveIntegerField()
-	premio_fecha_3 = models.PositiveIntegerField()
-	numero_ganador_1_fecha_1 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_2_fecha_1 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_3_fecha_1 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_1_fecha_2 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_2_fecha_2 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_3_fecha_2 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_1_fecha_3 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_2_fecha_3 = models.PositiveIntegerField(blank=True, null=True)
-	numero_ganador_3_fecha_3 = models.PositiveIntegerField(blank=True, null=True)
-	estado_fecha_1 = models.CharField(max_length=2, choices=ESTADOS_FECHAS, default=JUEGO)
-	estado_fecha_2 = models.CharField(max_length=2, choices=ESTADOS_FECHAS, default=JUEGO)
-	estado_fecha_3 = models.CharField(max_length=2, choices=ESTADOS_FECHAS, default=JUEGO)
-	estado_sorteo = models.CharField(max_length=2, choices=ESTADOS_SORTEOS, default=INSCRIPCIONES)
+	estado_sorteo_2 = models.CharField(max_length=2, choices=ESTADOS_FECHAS, default=JUEGO)
+	premio_sorteo_2 = models.PositiveIntegerField()
+	numero_ganador_1_sorteo_2 = models.PositiveIntegerField(blank=True, null=True)
+	numero_ganador_2_sorteo_2 = models.PositiveIntegerField(blank=True, null=True)
+	numero_ganador_3_sorteo_2 = models.PositiveIntegerField(blank=True, null=True)
+	ganador_sorteo_2 = models.ManyToManyField(Ganador, related_name='ganador_sorteo_2', blank=True)
+	
+	premio_registro_compras = models.PositiveIntegerField()
+	ganador_registro_compras = models.ManyToManyField(Ganador, related_name='ganador_sorteo_compras', blank=True)
 
 	def __unicode__(self):
-		return str(self.id) + " - " + self.estado_sorteo
-	def nombre_sorteo(self):
-		return str(self.id) + " - " + self.estado_sorteo
+		return self.nombre + " - " + self.estado_sorteo
 
 class Concursante(models.Model):
-	nombre = models.CharField(max_length=100)
-	correo = models.EmailField()
-	direccion = models.CharField(max_length=255)
-	telefono = models.CharField( max_length = 255 )
-	establecimiento = models.CharField( max_length = 255, blank = True )
+	establecimiento = models.CharField(max_length=255)
 	numero_ganador_1 = models.PositiveIntegerField()
 	numero_ganador_2 = models.PositiveIntegerField()
 	numero_ganador_3 = models.PositiveIntegerField()
 	acepto_terminos = models.BooleanField( default = False )
 	sorteo = models.ForeignKey(Sorteo)
+	residente = models.ForeignKey(Residente)
 
 	def __unicode__(self):
-		return self.nombre
+		return self.residente.nombre + ' - ' + self.sorteo.nombre
 
-# @receiver(post_save, sender=Concursante, dispatch_uid="identificador_unico", weak=False)
-def envio_correo_terminos(sender, **kwargs):
+	def nombre_concursante(self):
+		return self.residente.nombre
+	def correo_concursante(self):
+		return self.residente.correo
+
+def agregar_a_lista_de_correo(sender, **kwargs):
 	
-	concursante = kwargs.get('instance')
-	nombre_concursante = concursante.nombre
-	email_concursante = concursante.correo	
-	acepto_terminos = concursante.acepto_terminos
-	link_terminos_concursante = "http://comunidadbogota.com/ruperton/terminos/%s/" % concursante.id
+	residente = kwargs.get('instance')
+	nombre_residente = residente.nombre
+	
+	
+# @receiver(post_save, sender=Concursante, dispatch_uid="identificador_unico", weak=False)
+# def envio_correo_terminos(sender, **kwargs):
+	
+# 	concursante = kwargs.get('instance')
+# 	nombre_concursante = concursante.nombre
+# 	email_concursante = concursante.correo	
+# 	acepto_terminos = concursante.acepto_terminos
+# 	link_terminos_concursante = "http://comunidadbogota.com/ruperton/terminos/%s/" % concursante.id
 
-	# Enviamos un correo electronico con la informacion del concursante y el link de los terminos:
-	if not acepto_terminos:
-		try:
-			mandrill_client = mandrill.Mandrill('_SoGpYeWNJ0p3ziJ1Hn75g')
-			template_content = [
-				{'content':nombre_concursante, 'name':'nombre_concursante'},
-				{'content':link_terminos_concursante, 'name':'link_terminos'},
-			]
-			message = {
-				'to':[
-					{
-						'email': email_concursante,
-						'name': nombre_concursante,
-						'type': 'to'
-					}
-				],
-				'merge_language':'handlebars',
-				'from_email':'andres@cpsingenieria.co',
-				'merge_vars':[
-					{
-						'rcpt':email_concursante,
-						'vars':[
-							{'content':nombre_concursante, 'name':'nombre_concursante'},
-							{'content':link_terminos_concursante, 'name':'link_terminos'},
-						]
-					}
-				]
-			}
-			result = mandrill_client.messages.send_template(template_name='Test_cb_terminos', 
-				template_content=template_content, message=message, async=False)
-		except mandrill.Error, e:
-			print 'A mandrill error occurred: %s - %s' % (e.__class__, e)
+# 	# Enviamos un correo electronico con la informacion del concursante y el link de los terminos:
+# 	if not acepto_terminos:
+# 		try:
+# 			mandrill_client = mandrill.Mandrill('_SoGpYeWNJ0p3ziJ1Hn75g')
+# 			template_content = [
+# 				{'content':nombre_concursante, 'name':'nombre_concursante'},
+# 				{'content':link_terminos_concursante, 'name':'link_terminos'},
+# 			]
+# 			message = {
+# 				'to':[
+# 					{
+# 						'email': email_concursante,
+# 						'name': nombre_concursante,
+# 						'type': 'to'
+# 					}
+# 				],
+# 				'merge_language':'handlebars',
+# 				'from_email':'andres@cpsingenieria.co',
+# 				'merge_vars':[
+# 					{
+# 						'rcpt':email_concursante,
+# 						'vars':[
+# 							{'content':nombre_concursante, 'name':'nombre_concursante'},
+# 							{'content':link_terminos_concursante, 'name':'link_terminos'},
+# 						]
+# 					}
+# 				]
+# 			}
+# 			result = mandrill_client.messages.send_template(template_name='Test_cb_terminos', 
+# 				template_content=template_content, message=message, async=False)
+# 		except mandrill.Error, e:
+# 			print 'A mandrill error occurred: %s - %s' % (e.__class__, e)
 
-post_save.connect(envio_correo_terminos, sender=Concursante, dispatch_uid="identificador_unico", weak=False)
+# post_save.connect(envio_correo_terminos, sender=Concursante, dispatch_uid="identificador_unico", weak=False)
